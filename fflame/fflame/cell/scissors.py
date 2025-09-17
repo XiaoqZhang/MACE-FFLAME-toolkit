01890cdfdb9950f835e842b575bef0876c5cde54
Xiaoqi Zhang <90387077+XiaoqZhang@users.noreply.github.com>
2025-09-17 10:01:31 +0200

    Add docstring to cell/scissors.py

--- fflame/fflame/cell/scissors.py
+++ fflame/fflame/cell/scissors.py
@@ -1,3 +1,15 @@
+"""
+Example
+-------
+>>> from ase.io import read
+>>> from fflame.cell.scissors import LigandScissor
+>>> 
+>>> mof = read("HKUST1.cif")
+>>> scissors = LigandScissor(mof)
+>>> linker = scissors.extract_linkers(index=0, threshold=2.0, returntype="ase")
+>>> flag, optimized_linker = scissors.optimize_linker(linker, keep_origin=False, method="vesta")
+"""
+
 from moffragmentor import MOF
 from ase.io import read, write
 from pymatgen.io.ase import AseAtomsAdaptor
@@ -23,7 +35,20 @@ with open(os.path.join(file_folder, "../data/tuned_vesta.yml"), "r", encoding="u
     _VESTA_CUTOFFS = yaml.load(handle, Loader=yaml.UnsafeLoader)  
 
 class LigandScissor:
-    def __init__(self, mof):
+    """
+    A class for extracting and optimizing linker fragments from MOFs.
+
+    Parameters
+    ----------
+    mof : pymatgen.Structure or ase.Atoms
+        A MOF structure from which linkers will be extracted.
+
+    Raises
+    ------
+    TypeError
+        If `mof` is not a supported type.
+    """
+    def __init__(self, mof: Union[Structure, Atoms]):
         if isinstance(mof, Structure) or isinstance(mof, Atoms):
             random_name = os.path.join(file_folder, f"{int(time.time())}-{random.randint(0, 2**32 - 1)}.cif")
             write(random_name, mof)
@@ -33,18 +58,46 @@ class LigandScissor:
             raise TypeError("Unsupported type for mof. ")
         
     def fragments(self):
+        """Return the MOF fragments produced by `moffragmentor`."""
         return self.mof.fragment()
     
     def num_linkers(self):
+        """Return the number of linkers in the MOF."""
         return len(self.fragments().linkers)
     
     def linkers(self):
+        """Return the list of linkers extracted by `moffragmentor`."""
         return self.fragments().linkers   
     
     def lattice(self):
+        """Return the lattice of the MOF structure."""
         return self.mof.structure.lattice
     
-    def extract_linkers(self, index, threshold, returntype):
+    def extract_linkers(
+        self, index: int, threshold: float, returntype: str
+    ) -> Union[Structure, Atoms]:
+        """
+        Extract a linker molecule from the MOF.
+
+        Parameters
+        ----------
+        index : int
+            Index of the linker to extract.
+        threshold : float
+            Padding distance (Å) around the extracted linker.
+        returntype : {"pym", "ase"}
+            Output type: "pym" for pymatgen Structure, "ase" for ASE Atoms.
+
+        Returns
+        -------
+        Structure or Atoms
+            The extracted linker in the chosen format.
+
+        Raises
+        ------
+        ValueError
+            If `returntype` is not one of {"pym", "ase"}.
+        """
         lkmol = self.linkers()[index].molecule
         
         coords = np.array([site.coords for site in lkmol.sites])
@@ -67,7 +120,34 @@ class LigandScissor:
             lkatom = AseAtomsAdaptor.get_atoms(lkstruc)
             return lkatom
         
-    def optimize_linker(self, extracted_linker, keep_origin, method, **kwargs):
+    def optimize_linker(
+        self, extracted_linker: Union[Structure, Atoms], keep_origin: bool, method: str, **kwargs
+    ) -> Union[Tuple[bool, Atoms], Tuple[bool, Atoms, Atoms]]:
+        """
+        Optimize an extracted linker by adding hydrogens and relaxing geometry with XTB.
+
+        Parameters
+        ----------
+        extracted_linker : Structure or Atoms
+            The extracted linker structure to optimize.
+        keep_origin : bool
+            Whether to also return the original, unmodified structure.
+        method : {"vesta", "crystal"}
+            Method used for neighbor graph construction:
+            - "vesta" : Use tuned VESTA cutoffs.
+            - "crystal" : Use CrystalNN environment.
+        **kwargs :
+            Extra arguments passed to the graph constructor.
+            For "vesta", accepts "scaling" (float) to scale cutoff values.
+
+        Returns
+        -------
+        tuple
+            If keep_origin=True:
+                (flag, optimized_atoms, original_atoms)
+            Else:
+                (flag, optimized_atoms)
+        """
         
         if isinstance(extracted_linker, Atoms):
             extracted_linker = AseAtomsAdaptor.get_structure(extracted_linker)
@@ -79,6 +159,8 @@ class LigandScissor:
             graph_method = CutOffDictNN(cutoffs)
         elif method == 'crystal':
             graph_method = CrystalNN(**kwargs)
+        else:
+            raise ValueError("Invalid method. Choose 'vesta' or 'crystal'.")
             
         graph_linker = StructureGraph.with_local_env_strategy(extracted_linker, graph_method)
         _, h_pos = O_site_adding_hydrogen(extracted_linker, graph_linker)._get_O_charge()
